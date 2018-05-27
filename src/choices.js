871add85e80eb10c83fc76e796fc86f3af0e7a89
fs <xcorter@mail.ru>
2018-05-27 23:04:36 +0700

    master add new audio

--- src/choices.js
+++ src/choices.js
@@ -1,11 +1,12 @@
 import _ from 'lodash';
+import * as Keys from './constants/keys';
 
 const tests = [
   {
     "id": 1,
     "source": "/audio/1.mp3",
     "success": null,
-    "key": "C Maj",
+    "key": Keys.C_MAJ,
     "choices": [
       {"is_correct": false, "text": "Тоника"},
       {"is_correct": false, "text": "Доминанта"},
@@ -16,7 +17,7 @@ const tests = [
     "id": 2,
     "source": "/audio/2.mp3",
     "success": null,
-    "key": "C Maj",
+    "key": Keys.C_MAJ,
     "choices": [
       {"is_correct": false, "text": "Тоника"},
       {"is_correct": true, "text": "Доминанта"},
@@ -27,7 +28,7 @@ const tests = [
     "id": 3,
     "source": "/audio/c_ton.mp3",
     "success": null,
-    "key": "C Maj",
+    "key": Keys.C_MAJ,
     "choices": [
       {"is_correct": true, "text": "Тоника"},
       {"is_correct": false, "text": "Доминанта"},
@@ -38,7 +39,7 @@ const tests = [
     "id": 4,
     "source": "/audio/d_ton.mp3",
     "success": null,
-    "key": "D Maj",
+    "key": Keys.D_MAJ,
     "choices": [
       {"is_correct": true, "text": "Тоника"},
       {"is_correct": false, "text": "Доминанта"},
@@ -49,7 +50,7 @@ const tests = [
     "id": 5,
     "source": "/audio/d_sub.mp3",
     "success": null,
-    "key": "D Maj",
+    "key": Keys.D_MAJ,
     "choices": [
       {"is_correct": false, "text": "Тоника"},
       {"is_correct": false, "text": "Доминанта"},
@@ -60,7 +61,7 @@ const tests = [
     "id": 6,
     "source": "/audio/d_dom.mp3",
     "success": null,
-    "key": "D Maj",
+    "key": Keys.D_MAJ,
     "choices": [
       {"is_correct": false, "text": "Тоника"},
       {"is_correct": true, "text": "Доминанта"},
@@ -71,7 +72,7 @@ const tests = [
     "id": 7,
     "source": "/audio/a_ton.mp3",
     "success": null,
-    "key": "A Min",
+    "key": Keys.A_MIN,
     "choices": [
       {"is_correct": true, "text": "Тоника"},
       {"is_correct": false, "text": "Доминанта"},
@@ -82,7 +83,7 @@ const tests = [
     "id": 8,
     "source": "/audio/a_sub.mp3",
     "success": null,
-    "key": "A Min",
+    "key": Keys.A_MIN,
     "choices": [
       {"is_correct": false, "text": "Тоника"},
       {"is_correct": false, "text": "Доминанта"},
@@ -93,13 +94,145 @@ const tests = [
     "id": 9,
     "source": "/audio/a_dom.mp3",
     "success": null,
-    "key": "A Min",
+    "key": Keys.A_MIN,
     "choices": [
       {"is_correct": false, "text": "Тоника"},
       {"is_correct": true, "text": "Доминанта"},
       {"is_correct": false, "text": "Субдоминанта"}
     ]
-  }
+  },
+  {
+    "id": 10,
+    "source": "/audio/c-guitar/guitar-c-d.mp3",
+    "success": null,
+    "key": Keys.C_MAJ,
+    "choices": [
+      {"is_correct": false, "text": "Тоника"},
+      {"is_correct": true, "text": "Доминанта"},
+      {"is_correct": false, "text": "Субдоминанта"}
+    ]
+  },
+  {
+    "id": 11,
+    "source": "/audio/c-guitar/guitar-c-t.mp3",
+    "success": null,
+    "key": Keys.C_MAJ,
+    "choices": [
+      {"is_correct": true, "text": "Тоника"},
+      {"is_correct": false, "text": "Доминанта"},
+      {"is_correct": false, "text": "Субдоминанта"}
+    ]
+  },
+  {
+    "id": 12,
+    "source": "/audio/c-guitar/guitar-c-s.mp3",
+    "success": null,
+    "key": Keys.C_MAJ,
+    "choices": [
+      {"is_correct": false, "text": "Тоника"},
+      {"is_correct": false, "text": "Доминанта"},
+      {"is_correct": true, "text": "Субдоминанта"}
+    ]
+  },
+  {
+    "id": 13,
+    "source": "/audio/d-guitar/guitar-d-d.mp3",
+    "success": null,
+    "key": Keys.D_MAJ,
+    "choices": [
+      {"is_correct": false, "text": "Тоника"},
+      {"is_correct": true, "text": "Доминанта"},
+      {"is_correct": false, "text": "Субдоминанта"}
+    ]
+  },
+  {
+    "id": 14,
+    "source": "/audio/d-guitar/guitar-d-t.mp3",
+    "success": null,
+    "key": Keys.D_MAJ,
+    "choices": [
+      {"is_correct": true, "text": "Тоника"},
+      {"is_correct": false, "text": "Доминанта"},
+      {"is_correct": false, "text": "Субдоминанта"}
+    ]
+  },
+  {
+    "id": 15,
+    "source": "/audio/d-guitar/guitar-d-s.mp3",
+    "success": null,
+    "key": Keys.D_MAJ,
+    "choices": [
+      {"is_correct": false, "text": "Тоника"},
+      {"is_correct": false, "text": "Доминанта"},
+      {"is_correct": true, "text": "Субдоминанта"}
+    ]
+  },
+  {
+    "id": 16,
+    "source": "/audio/e-guitar/guitar-e-d.mp3",
+    "success": null,
+    "key": Keys.E_MAJ,
+    "choices": [
+      {"is_correct": false, "text": "Тоника"},
+      {"is_correct": true, "text": "Доминанта"},
+      {"is_correct": false, "text": "Субдоминанта"}
+    ]
+  },
+  {
+    "id": 17,
+    "source": "/audio/e-guitar/guitar-e-t.mp3",
+    "success": null,
+    "key": Keys.E_MAJ,
+    "choices": [
+      {"is_correct": true, "text": "Тоника"},
+      {"is_correct": false, "text": "Доминанта"},
+      {"is_correct": false, "text": "Субдоминанта"}
+    ]
+  },
+  {
+    "id": 18,
+    "source": "/audio/e-guitar/guitar-e-s.mp3",
+    "success": null,
+    "key": Keys.E_MAJ,
+    "choices": [
+      {"is_correct": false, "text": "Тоника"},
+      {"is_correct": false, "text": "Доминанта"},
+      {"is_correct": true, "text": "Субдоминанта"}
+    ]
+  },
+  {
+    "id": 19,
+    "source": "/audio/f-guitar/guitar-f-d.mp3",
+    "success": null,
+    "key": Keys.F_MAJ,
+    "choices": [
+      {"is_correct": false, "text": "Тоника"},
+      {"is_correct": true, "text": "Доминанта"},
+      {"is_correct": false, "text": "Субдоминанта"}
+    ]
+  },
+  {
+    "id": 20,
+    "source": "/audio/f-guitar/guitar-f-t.mp3",
+    "success": null,
+    "key": Keys.F_MAJ,
+    "choices": [
+      {"is_correct": true, "text": "Тоника"},
+      {"is_correct": false, "text": "Доминанта"},
+      {"is_correct": false, "text": "Субдоминанта"}
+    ]
+  },
+  {
+    "id": 21,
+    "source": "/audio/f-guitar/guitar-f-s.mp3",
+    "success": null,
+    "key": Keys.F_MAJ,
+    "choices": [
+      {"is_correct": false, "text": "Тоника"},
+      {"is_correct": false, "text": "Доминанта"},
+      {"is_correct": true, "text": "Субдоминанта"}
+    ]
+  },
 ]
 
 export function getChoices() {
---
+++ src/constants/keys.js
@@ -0,0 +1,15 @@
+export const A_MAJ = 'E Maj';
+export const B_MAJ = 'B Maj';
+export const C_MAJ = 'C Maj';
+export const D_MAJ = 'D Maj';
+export const E_MAJ = 'E Maj';
+export const F_MAJ = 'F Maj';
+export const G_MAJ = 'G Maj';
+
+export const A_MIN = 'E Min';
+export const B_MIN = 'B Min';
+export const C_MIN = 'C Min';
+export const D_MIN = 'D Min';
+export const E_MIN = 'E Min';
+export const F_MIN = 'F Min';
+export const G_MIN = 'G Min';
